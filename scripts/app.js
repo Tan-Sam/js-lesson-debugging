f2792f9f26fdbcc04beec1325513acd13f63ff48
sam <Tan-Sam@users.noreply.github.com>
2017-05-22 14:27:19 +0800

    finish debugging 2 js files.

--- scripts/app.js
+++ scripts/app.js
@@ -4,14 +4,14 @@ alert("hi");
 
 console.log("Hello");
 
-setTimeout(function(){ 
+setTimeout(function(){
   alert("Loaded"); 
 }, 0);
 
-setTimeout(function(){ 
-  alert("Hello"); 
+setTimeout(function(){
+  alert("Hello");
 }, 500);
 
-setTimeout(function(){ 
-  alert("ROAR"); 
+setTimeout(function(){
+  alert("ROAR");
 }, 2000);
